0fa2cdc20a5077a4e3e8c04be6be7fe1bf28e631
Cw <sumitrana5858@gmail.com>
2026-04-18 11:17:14 +0530

    fix alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  server: {
-    host: true,
-    allowedHosts: ["gleam-shop-world.onrender.com"]
-  }
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
