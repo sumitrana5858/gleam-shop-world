10f86ad709789d6f85202d2cf030643d9279c3b1
Cw <sumitrana5858@gmail.com>
2026-04-18 22:10:01 +0530

    fix router export

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
-import { router } from './router'
+import { getRouter } from './router'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = getRouter()
+
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error("Root element not found")
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
